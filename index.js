a27c717f9998d7054bba8b8b2302d6827307c192
abenaTenkorang <abenagy16@gmail.com>
2022-11-14 16:35:05 +0000

    Added js file to navbar on mobile-menu

---
+++ index.js
@@ -0,0 +1,23 @@
+const menu = document.querySelector('.menu');
+const menuItem = document.querySelectorAll('.menu-item');
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon');
+const hamburger = document.querySelector('.hamburger');
+
+function toggleMenu() {
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+  } else {
+    menu.classList.add('showMenu');
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
+  }
+}
+
+hamburger.addEventListener('click', toggleMenu);
+
+menuItem.forEach((menuItem) => {
+  menuItem.addEventListener('click', toggleMenu);
+});
