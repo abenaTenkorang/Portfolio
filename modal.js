903dd05c2505a40e31e77eaf2000daf614d1bed5
abenaTenkorang <abenagy16@gmail.com>
2022-11-16 19:38:22 +0000

    Modal.js file added

---
+++ modal.js
@@ -0,0 +1,55 @@
+const data = [
+    {
+      id: "project-1",
+      title: "Tonic",
+      canopy: "Canopy",
+      html: "html",
+      css: "css",
+      javascript: "javasctipt",
+      image: "images/Screenshot-to-list-app.png",
+      about:
+        "This is a to do list app that helps you organize your dayly takes. After adding a task you can edit it and delete it once complete.",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, fugiat maiores quam amet hic dolorum unde eaque explicabo sapiente asperiores voluptatibus vel. Neque deserunt sunt vel ducimus voluptatem tenetur quam incidunt esse sequi enim labore beatae iure minus dolorem nam commodi libero atque, voluptatum alias accusantium, molestiae maiores voluptatibus error. Cupiditate, enim nobis. Expedita deserunt et eaque quisquam nostrum ducimus quasi, accusantium facilis, quibusdam quia, accusamus excepturi voluptate minus consectetur dolore adipisci delectus! Facere officiis corrupti autem sequi quod atque?",
+    },
+    {
+      id: "project-2",
+      title: "Multi-Post Stories",
+      image: "images/Snapshoot2.png",
+      canopy: "Canopy",
+      html: "html",
+      css: "css",
+      javascript: "javasctipt",
+      about:
+        "This is a to do list app that helps you organize your dayly takes. After adding a task you can edit it and delete it once complete.",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, fugiat maiores quam amet hic dolorum unde eaque explicabo sapiente asperiores voluptatibus vel. Neque deserunt sunt vel ducimus voluptatem tenetur quam incidunt esse sequi enim labore beatae iure minus dolorem nam commodi libero atque, voluptatum alias accusantium, molestiae maiores voluptatibus error. Cupiditate, enim nobis. Expedita deserunt et eaque quisquam nostrum ducimus quasi, accusantium facilis, quibusdam quia, accusamus excepturi voluptate minus consectetur dolore adipisci delectus! Facere officiis corrupti autem sequi quod atque?",
+    },
+    {
+      id: "project-3",
+      title: "Tonic",
+      canopy: "Canopy",
+      image: "images/Snapshoot3.png",
+      html: "html",
+      css: "css",
+      javascript: "javasctipt",
+      about:
+        "This is a to do list app that helps you organize your dayly takes. After adding a task you can edit it and delete it once complete.",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, fugiat maiores quam amet hic dolorum unde eaque explicabo sapiente asperiores voluptatibus vel. Neque deserunt sunt vel ducimus voluptatem tenetur quam incidunt esse sequi enim labore beatae iure minus dolorem nam commodi libero atque, voluptatum alias accusantium, molestiae maiores voluptatibus error. Cupiditate, enim nobis. Expedita deserunt et eaque quisquam nostrum ducimus quasi, accusantium facilis, quibusdam quia, accusamus excepturi voluptate minus consectetur dolore adipisci delectus! Facere officiis corrupti autem sequi quod atque?",
+    },
+    {
+      id: "project-4",
+      title: "Multi-Post Stories",
+      canopy: "Canopy",
+      image: "images/Snapshoot4.png",
+      html: "html",
+      css: "css",
+      javascript: "javasctipt",
+      about:
+        "This is a to do list app that helps you organize your dayly takes. After adding a task you can edit it and delete it once complete.",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, fugiat maiores quam amet hic dolorum unde eaque explicabo sapiente asperiores voluptatibus vel. Neque deserunt sunt vel ducimus voluptatem tenetur quam incidunt esse sequi enim labore beatae iure minus dolorem nam commodi libero atque, voluptatum alias accusantium, molestiae maiores voluptatibus error. Cupiditate, enim nobis. Expedita deserunt et eaque quisquam nostrum ducimus quasi, accusantium facilis, quibusdam quia, accusamus excepturi voluptate minus consectetur dolore adipisci delectus! Facere officiis corrupti autem sequi quod atque?",
+    },
+  ];
+  
